62b08410a139eee1a02464b3e46a15b84abcf51e
Robson Lima <rsl.robson@gmail.com>
2020-01-06 16:55:33 -0300

    Alterado método para exibir a quantidade de ciclos da execução do exemplo do desafio

--- src/br/com/alura/maratona/Main.java
+++ src/br/com/alura/maratona/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        calculaPara(22);
+        calculaCiclos(999999);
 
 //        while (scanner.hasNextLine()) {
 //            int i = scanner.nextInt();
@@ -19,13 +19,15 @@ public class Main {
 //        }
     }
 
-    private static void calculaPara(int n) {
-        System.out.println(n);
+    private static void calculaCiclos(int n) {
+        int contagem_ciclos = 1;
         while (n != 1) {
             if (n % 2 == 1) n = n * 3 + 1;
             else n = n / 2;
 
-            System.out.println(n);
+            contagem_ciclos++;
         }
+
+        System.out.println("Ciclos: " + contagem_ciclos);
     }
 }
